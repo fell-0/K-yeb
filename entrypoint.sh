3b33ee4f1355c9086bcb884f24e6159d621d25ff
fell-0 <130829961+fell-0@users.noreply.github.com>
2023-05-05 08:41:39 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 base64 -d config > config.json
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
+UUID=${UUID:-'1a191a8d-2c7b-4044-9087-fb968beb7c6d'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/1a191a8d-2c7b-4044-9087-fb968beb7c6d-vmess'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/1a191a8d-2c7b-4044-9087-fb968beb7c6d-vless'}
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" config.json
 sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" /etc/nginx/nginx.conf
 sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
